4d190dc6c0f92119fcbc8baaf4fb55acd25fbfe2
liao <liaomovie2@gmail.com>
2018-10-18 18:02:08 +0800

    tweak
    [skip ci]

--- R/init-project.R
+++ R/init-project.R
@@ -1,8 +1,10 @@
 #' Initialize current directory as a bookdown project
 #'
+#' @param dir The path to the dircetory to initialize as a project
 #' @export
 init_proj <- function(dir = ".") {
-  rproj <- system.file("init-project/ntu-bookdown.Rproj.txt", package = "ntuthesis")
+  rproj <- system.file("init-project/ntu-bookdown.Rproj.txt",
+                       package = "ntuthesis")
   file.copy(from = rproj, to = dir)
   file.rename(from = "ntu-bookdown.Rproj.txt",
               to = paste0(basename(getwd()), ".Rproj"))
