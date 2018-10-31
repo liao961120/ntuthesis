49d37d09985e37ef8fdc494b5a96ed1d74f7ff28
liao <liaomovie2@gmail.com>
2018-10-31 17:59:40 +0800

    tweak

--- R/init-project.R
+++ R/init-project.R
@@ -1,6 +1,14 @@
 #' Initialize current directory as a bookdown project
 #'
-#' @param dir The path to the dircetory to initialize as a project
+#' @param dir The path to the dircetory to initialize as
+#'   a project.
+#'
+#' @examples
+#' \dontrun{
+#' setwd("~/my-thesis")
+#' init_proj()
+#' }
+#'
 #' @export
 init_proj <- function(dir = ".") {
   rproj <- system.file("init-project/ntu-bookdown.Rproj.txt",
