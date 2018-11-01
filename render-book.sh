28e63841b3d9847231930204ee4daaa532d08813
liao <liaomovie2@gmail.com>
2018-11-01 22:01:06 +0800

    move example-thesis
    build ignore it

--- render-book.sh
+++ render-book.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd inst/example-thesis
+cd example-thesis
 Rscript -e 'bookdown::render_book("index.Rmd", "bookdown::gitbook")'
 cd -
 
-cp -r inst/example-thesis/_book .
+cp -r example-thesis/_book .
