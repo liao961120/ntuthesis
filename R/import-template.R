bc86668881778359fbc947699ac90352687eff4e
liao <liaomovie2@gmail.com>
2018-11-12 10:41:27 +0800

    Remove <name>.Rmd when importing template

--- R/import-template.R
+++ R/import-template.R
@@ -15,4 +15,6 @@ import_template <- function(name = "My_Thesis") {
   rmarkdown::draft(name, edit = F,
                    template = "ntu_bookdown",
                    package = "ntuthesis")
+  rmd_file <- paste0(name,"/",name,".Rmd")
+  if (file.exists(rmd_file)) file.remove(rmd_file)
 }
