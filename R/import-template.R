1c4fe616cd46da15337e6a92a55d93dd784ed8cb
liao <liaomovie2@gmail.com>
2018-11-12 10:41:27 +0800

    Remove <name>.Rmd when importing template

--- R/import-template.R
+++ R/import-template.R
@@ -4,6 +4,8 @@
 #'   wrapper of \code{\link{rmarkdown::draft}}.
 #'
 #' @param name The name of the thesis project to create.
+#'   Note `name` can't be path, i.e, `name` can't contain
+#'   `/`s.
 #'
 #' @examples
 #' \dontrun{
@@ -15,4 +17,6 @@ import_template <- function(name = "My_Thesis") {
   rmarkdown::draft(name, edit = F,
                    template = "ntu_bookdown",
                    package = "ntuthesis")
+  rmd_file <- paste0(name,"/",name,".Rmd")
+  if (file.exists(rmd_file)) file.remove(rmd_file)
 }
