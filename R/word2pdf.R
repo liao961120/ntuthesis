6499b6388fe2a271b574160667a7366dc61d415e
liao <liaomovie2@gmail.com>
2019-02-13 22:35:02 +0800

    export word2pdf()

--- R/word2pdf.R
+++ R/word2pdf.R
@@ -1,6 +1,18 @@
-#' Pandoc Wrapper
-#' @keywords internal
-pandoc <- function(infile = 'word.docx', outfile = 'word.pdf') {
+#' Compile Word into PDF
+#'
+#' Pandoc Wrapper function
+#'
+#' @param infile String. File path to the input word document.
+#'   Must be in \code{.docx} format.
+#' @param outfile String. File path of the output pdf document
+#'   to be generated. Must be in \code{.pdf} format.
+#' @importFrom tools file_ext
+#' @export
+word2pdf <- function(infile = 'word.docx', outfile = 'word.pdf') {
+  stopifnot(file.exists(infile))
+  if (file_ext(infile) != 'docx') stop('Input file not `docx` format')
+  if (file_ext(outfile) != 'pdf') stop('Output file not `pdf` format')
+
   pandoc <- rmarkdown:::find_pandoc()
   if (!is.null(pandoc$dir)) {
     pandoc_exe <- list.files(pandoc$dir, full.names = T)[[1]]
@@ -10,5 +22,5 @@ pandoc <- function(infile = 'word.docx', outfile = 'word.pdf') {
     stop("Can't find Pandoc", "\n",
          "Please install Rstudio or Pandoc first")
   }
-  system2(pandoc_exe, args = c(infile, '-o', outfile))
+  system2(pandoc_exe, args = c(infile, '-o', outfile, '--pdf-engine=xelatex'))
 }
