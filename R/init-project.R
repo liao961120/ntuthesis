76c3b2065a0a6ebd2928a0dd733814c66f889297
liao <liaomovie2@gmail.com>
2019-05-08 21:45:26 +0800

    download font globally, not every time when template is imported. #2 #4

--- R/init-project.R
+++ R/init-project.R
@@ -2,9 +2,6 @@
 #'
 #' @param dir The path to the dircetory to initialize as
 #'   a project.
-#' @param download_font Logical. Whether to download \code{kaiu.ttf}
-#'   for CJK mainfont & \code{NotoSansMonoCJKtc.otf} for CJK mono font.
-#'   Defaults to \code{TRUE}.
 #'
 #' @examples
 #' \dontrun{
@@ -13,13 +10,10 @@
 #' }
 #'
 #' @export
-init_proj <- function(dir = ".", download_font = TRUE) {
+init_proj <- function(dir = ".") {
   rproj <- system.file("init-project/ntu-bookdown.Rproj.txt",
                        package = "ntuthesis")
   file.copy(from = rproj, to = dir)
   file.rename(from = "ntu-bookdown.Rproj.txt",
               to = paste0(basename(getwd()), ".Rproj"))
-
-  # Download Chinese font dependencies
-  if (download_font) donwload_font_all()
 }
