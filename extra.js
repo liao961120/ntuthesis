3a60b53946694c2b2d27e52a31b9b249452ac42e
liao <liao961120@gmail.com>
2021-02-17 23:20:04 +0800

    deprecrated

---
+++ extra.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  var links = document.getElementsByTagName('a');
+  for (var i = 0; i < links.length; i++) {
+    if (/^(https?:)?\/\//.test(links[i].getAttribute('href'))) {
+      links[i].target = '_blank';
+    }
+  }
+})();
